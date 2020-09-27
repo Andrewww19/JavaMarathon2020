9bdb91610aa617868ae643745525c9a70b0a0b8f
Andrew Plishchuk <plishchuk@gmail.com>
2020-09-27 15:46:16 +0300

    complete day 5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+    	Car car = new Car();
+    	car.setYearOfProduction(1995);
+    	car.setColor("red");
+    	car.setModel("BMW 525");
+    	System.out.println(car.getYearOfProduction());
+    	System.out.println(car.getColor());
+    	System.out.println(car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,6 +2,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+    	Motorbike motorbike = new Motorbike(2020, "Black", "Yamaha Some model:)");
+    	System.out.println(motorbike.getYearOfProduction());
+    	System.out.println(motorbike.getColor());
+    	System.out.println(motorbike.getModel());
     }
 }
