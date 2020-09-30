534821953d843ac94900e027e6c4b76e3ca8d985
Andrew Plishchuk <plishchuk@gmail.com>
2020-09-30 11:58:27 +0300

    complete day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+    	StringBuilder sb = new StringBuilder();
+    	long start;
+    	long end;
+    	
+    	start = System.currentTimeMillis();
+    	for (int i = 0; i < 20000; i++) {//6622
+			sb.append(i + " ");
+		}
+    	end = System.currentTimeMillis();
+    	System.out.println(end - start);
+    	System.out.println(sb.toString());
+    	
+    	String str = "";
+    	start = System.currentTimeMillis();
+    	for (int i = 0; i < 20000; i++) {
+			str = str + i + " ";
+		}
+    	end = System.currentTimeMillis();
+    	System.out.println(end - start);
+    	System.out.println(str);
     }
 }
