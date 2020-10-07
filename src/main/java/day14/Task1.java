51b6e445c9639290c9f7c452dad8cdb8398ac6a5
Andrew Plishchuk <plishchuk@gmail.com>
2020-10-07 19:37:18 +0300

    complete day 14 tasks

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,35 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.IIOException;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        String path = "src\\main\\java\\day14\\text";
+        printSumDigits(new File(path));
+    }
+    public static int printSumDigits(File file) {
+        int result = 0;
+        int counter = 0;
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNext()) {
+                result += sc.nextInt();
+                counter++;
+            }
+            if (counter != 10) {
+                throw new IOException();
+            }
+            System.out.println(result);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Некорректный входной файл");
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,38 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        File file = new File("src\\main\\java\\day14\\people.txt");
+        System.out.println(parseFileToObjList(file));
+    }
+    public static List<Person> parseFileToObjList(File file){
+        List<Person> list = new ArrayList<>();
+        
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNext()) {
+                String tmp1 = sc.next();
+                int tmp2 = sc.nextInt();
+                if (tmp2 < 0) {
+                    throw new IOException();
+                } else {
+                    list.add(new Person(tmp1, tmp2));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Некорректный входной файл");
+            e.printStackTrace();
+        }
+        
+        
+        return list;
     }
 }
