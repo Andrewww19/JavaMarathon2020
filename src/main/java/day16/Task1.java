859684649e8b1fcdb3c49deba5e2d53c644775e9
Andrew Plishchuk <plishchuk@gmail.com>
2020-10-08 21:46:23 +0300

    COMPLETE DAY16 TASKS

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,26 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String path = "src\\main\\java\\day14\\text";
+        printResult(new File(path));
+    }
+    public static void printResult(File file) {
 
+        try(Scanner sc = new Scanner(file)) {
+            int result = 0;
+            int counter = 0;
+            while(sc.hasNext()) {
+                result += sc.nextInt();
+                counter ++;
+            }
+            System.out.print(String.format((double) result / counter + " --> %.3f", (double) result / counter));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,60 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        writeToFirstFile(file1);
+        writeToSeconfFile(file1, file2);
+        printResult(file2);
+    }
 
+    public static void writeToFirstFile(File file) {
+        try (PrintWriter pw = new PrintWriter(file)) {
+            Random random = new Random();
+            for (int i = 0; i < 1000; i++) {
+                pw.println(random.nextInt(101));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToSeconfFile(File fileIn, File fileOut) {
+        try (Scanner sc = new Scanner(fileIn); PrintWriter pw = new PrintWriter(fileOut)) {
+            int counter = 0;
+            int result = 0;
+            while (sc.hasNext()) {
+                result += sc.nextInt();
+                counter++;
+                if (counter % 20 == 0) {
+                    pw.println((double) result / 20);
+                    result = 0;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printResult(File file) {
+        try (Scanner sc = new Scanner(file)) {
+            double result = 0;
+            while (sc.hasNext()) {
+                result += Double.parseDouble(sc.next());
+            }
+            System.out.println((int) result);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
