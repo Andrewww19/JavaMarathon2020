87140de59d3f9d5d76c8f8e327d9f92830fa3e2a
Andrew Plishchuk <plishchuk@gmail.com>
2020-10-08 15:12:33 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,32 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File readingFile = new File("src//main//resources//shoes.csv");
+        File writingFile = new File("src//main//resources//missing_shoes.txt");
+        readWriteToFile(readingFile, writingFile);
+    }
+
+    public static void readWriteToFile(File inFile, File outFile) {
+
+        try {
+            PrintWriter pw = new PrintWriter(outFile);
+            Scanner sc = new Scanner(inFile);
+            while (sc.hasNext()) {
+                String[] arr = sc.nextLine().split(";");
+                if (Integer.parseInt(arr[2]) == 0) {
+                    pw.println(String.format("%s, %s, %s", arr[0], arr[1], arr[2]));
+                }
+            }
+            sc.close();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
